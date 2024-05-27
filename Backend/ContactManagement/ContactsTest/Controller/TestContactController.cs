6d09d2c52eb390470f04d78ce46c96e6e3082c9f
Pramod-naik47 <pramodnaik221@gmail.com>
2024-05-27 16:46:23 +0530

    Demo test cases

---
+++ Backend/ContactManagement/ContactsTest/Controller/TestContactController.cs
@@ -0,0 +1,44 @@
+﻿using ContactManagement.Controllers;
+using ContactManagement.Interface;
+using ContactsTest.MockData;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ContactsTest.Controller;
+//Example test cases
+public class TestContactController
+{
+    [Fact]
+    public async Task GetContactList_200Status()
+    {
+        //Arrange
+        var contactService = new Mock<IContact>();//Creating mock service
+        contactService.Setup(c => c.GetContactsList()).ReturnsAsync(ContactsMockData.GetContactList());//using dummy data
+
+        //Act
+        var sut = new ContactsController(contactService.Object);
+        var result = await sut.GetContacts();
+
+        //Assert
+        result.GetType().Should().Be(typeof(OkObjectResult));
+        ((OkObjectResult)result).StatusCode.Should().Be(200);
+    }
+
+
+    [Fact]
+    public async Task GetContactList_204Status()
+    {
+        //Arrange
+        var contactService = new Mock<IContact>();
+        contactService.Setup(c => c.GetContactsList()).ReturnsAsync(ContactsMockData.GetEmptyContactList());
+
+        //Act
+        var sut = new ContactsController(contactService.Object);
+        var result = await sut.GetContacts();
+
+        //Assert
+        result.GetType().Should().Be(typeof(NoContentResult));
+        ((NoContentResult)result).StatusCode.Should().Be(204);
+    }
+}
---
+++ Backend/ContactManagement/ContactsTest/MockData/ContactsMockData.cs
@@ -0,0 +1,39 @@
+﻿using ContactManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsTest.MockData;
+
+public static class ContactsMockData
+{
+    public static IEnumerable<ContactModel> GetContactList()
+    {
+        return new List<ContactModel>
+        {
+            new ContactModel
+            {
+                ContactId = "71415435-7126-4c6a-82ce-c8a22de0e3eb",
+                Name = "Pramod Naik",
+                Email = "pramod@gmail.com",
+                PhoneNumber = "7896584568",
+                Address = "Chilimbi"
+            },
+            new ContactModel
+            {
+                ContactId = "e041efe0-463d-452c-9c50-df92f97e648d",
+                Name = "Erik",
+                Email = "erk@gmail.com",
+                PhoneNumber = "7896584789",
+                Address = "Bangalore"
+            }
+        };
+    }
+
+    public static IEnumerable<ContactModel> GetEmptyContactList()
+    {
+        return Enumerable.Empty<ContactModel>();
+    }
+}
